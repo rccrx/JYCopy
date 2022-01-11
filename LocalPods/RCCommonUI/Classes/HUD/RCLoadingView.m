0ea8f12d5ab658689083edc968532cf652d0255e
rccrx <rccrx@qq.com>
2022-01-11 11:12:38 +0800

    M：修改loading动画-1

--- LocalPods/RCCommonUI/Classes/HUD/RCLoadingView.m
+++ LocalPods/RCCommonUI/Classes/HUD/RCLoadingView.m
@@ -11,20 +11,46 @@
 
 - (instancetype)init {
     if (self = [super init]) {
-        CALayer *layer = [[CALayer alloc] init];
-        layer.frame = CGRectMake(0, 0, self.intrinsicContentSize.width, self.intrinsicContentSize.height);
-        layer.backgroundColor = [UIColor colorWithRed:0 green:1 blue:1 alpha:0.7].CGColor;
-        layer.cornerRadius = CGRectGetHeight(layer.frame) * 0.5;
-        [self.layer addSublayer:layer];
+        CALayer *layer1 = [[CALayer alloc] init];
+        layer1.frame = CGRectMake(0, 0, 10, 10);
+        layer1.backgroundColor = [UIColor colorWithRed:104/255.0 green:239/255.0 blue:245/255.0 alpha:0.5].CGColor;
+        layer1.cornerRadius = CGRectGetHeight(layer1.frame) * 0.5;
+        [self.layer addSublayer:layer1];
+        CALayer *layer2 = [[CALayer alloc] init];
+        layer2.frame = CGRectMake(10, 0, 10, 10);
+        layer2.backgroundColor = [UIColor colorWithRed:255/255.0 green:82/255.0 blue:218/255.0 alpha:0.5].CGColor;
+        layer2.cornerRadius = CGRectGetHeight(layer2.frame) * 0.5;
+        [self.layer insertSublayer:layer2 below:layer1];
         
-        CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
-        animation.fromValue = @0.5;
-        animation.toValue = @1.5;
-        animation.autoreverses = YES;
-        animation.repeatCount = INFINITY;
-        animation.duration = 1;
-        animation.removedOnCompletion = NO;
-        [layer addAnimation:animation forKey:@"animation1"];
+        
+        CAKeyframeAnimation *translation1 = [CAKeyframeAnimation animationWithKeyPath:@"transform.translation.x"];
+        translation1.values = @[@0, @5, @10, @5, @0];
+        translation1.timingFunctions = @[[CAMediaTimingFunction functionWithControlPoints:0.8 :0 :1 :1], [CAMediaTimingFunction functionWithControlPoints:0 :0 :0.2 :1], [CAMediaTimingFunction functionWithControlPoints:0.8 :0 :1 :1], [CAMediaTimingFunction functionWithControlPoints:0 :0 :0.2 :1]]; //kCAMediaTimingFunctionEaseIn大概控制点是(0.42,0)，(1,1)，调大c1x使得开始更慢，使得停留在@0的时间比EaseIn长一点；以(0,0)，(0.2,1)代替EaseOut使得停留在@10的时间长一点
+        CAKeyframeAnimation *scale1 = [CAKeyframeAnimation animationWithKeyPath:@"transform.scale"];
+        scale1.values = @[@1, @0.55, @1, @1.15, @1];
+        scale1.timingFunctions = @[[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut], [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut], [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut], [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn]];
+        
+        CAAnimationGroup *group1 = [CAAnimationGroup animation];
+        group1.animations = @[translation1, scale1];
+        group1.repeatCount = INFINITY;
+        group1.duration = 1;
+        group1.removedOnCompletion = NO;
+        [layer1 addAnimation:group1 forKey:@"animationGroup"];
+        
+        
+        CAKeyframeAnimation *translation2 = [CAKeyframeAnimation animationWithKeyPath:@"transform.translation.x"];
+        translation2.values = @[@0, @-5, @-10, @-5, @0];
+        translation2.timingFunctions = @[[CAMediaTimingFunction functionWithControlPoints:0.8 :0 :1 :1], [CAMediaTimingFunction functionWithControlPoints:0 :0 :0.2 :1], [CAMediaTimingFunction functionWithControlPoints:0.8 :0 :1 :1], [CAMediaTimingFunction functionWithControlPoints:0 :0 :0.2 :1]];
+        CAKeyframeAnimation *scale2 = [CAKeyframeAnimation animationWithKeyPath:@"transform.scale"];
+        scale2.values = @[@1, @1.15, @1, @0.55, @1];
+        scale2.timingFunctions = @[[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut], [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn], [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut], [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut]];
+        
+        CAAnimationGroup *group2 = [CAAnimationGroup animation];
+        group2.animations = @[translation2, scale2];
+        group2.repeatCount = INFINITY;
+        group2.duration = group1.duration;
+        group2.removedOnCompletion = NO;
+        [layer2 addAnimation:group2 forKey:@"animationGroup"];
     }
     return self;
 }
